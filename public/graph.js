59c1bf2e957cc2e19492f8fd9f36c326e94c6959
Dan Book <grinnz@grinnz.com>
2019-04-27 08:16:44 -0400

    move graph data retrieval into function, update dist name input on click

--- public/graph.js
+++ public/graph.js
@@ -66,45 +66,53 @@ function create_graph(elements, graphstyle, root) {
     ],
     layout: layout
   });
+  cy.on('tap', 'node', function(event) {
+    var distname = event.target.data('label');
+    document.getElementById('form-dist-name').setAttribute('value', distname);
+  });
 }
 
-var params = new URLSearchParams(window.location.search.substring(1));
-var dist = params.get('dist');
-var graphstyle = params.get('style');
-var phase = params.get('phase');
-var recommends = params.get('recommends');
-var suggests = params.get('suggests');
-var perl_version = params.get('perl_version');
+function retrieve_graph() {
+  var params = new URLSearchParams(window.location.search.substring(1));
+  var dist = params.get('dist');
+  var graphstyle = params.get('style');
+  var phase = params.get('phase');
+  var recommends = params.get('recommends');
+  var suggests = params.get('suggests');
+  var perl_version = params.get('perl_version');
 
-var deps_url = new URL('/api/v1/deps', window.location.href);
-deps_url.searchParams.set('dist', dist);
-deps_url.searchParams.set('phase', 'runtime');
-if (phase === 'build') {
-  deps_url.searchParams.append('phase', 'configure');
-  deps_url.searchParams.append('phase', 'build');
-} else if (phase === 'test') {
-  deps_url.searchParams.append('phase', 'configure');
-  deps_url.searchParams.append('phase', 'build');
-  deps_url.searchParams.append('phase', 'test');
-}
-deps_url.searchParams.set('relationship', 'requires');
-if (recommends) { deps_url.searchParams.append('relationship', 'recommends'); }
-if (suggests) { deps_url.searchParams.append('relationship', 'suggests'); }
-if (perl_version !== null && perl_version !== '') {
-  deps_url.searchParams.set('perl_version', perl_version);
-}
-fetch(deps_url).then(function(response) {
-  if (response.ok) {
-    return response.json();
-  } else {
-    throw new Error(response.status + ' ' + response.statusText);
+  var deps_url = new URL('/api/v1/deps', window.location.href);
+  deps_url.searchParams.set('dist', dist);
+  deps_url.searchParams.set('phase', 'runtime');
+  if (phase === 'build') {
+    deps_url.searchParams.append('phase', 'configure');
+    deps_url.searchParams.append('phase', 'build');
+  } else if (phase === 'test') {
+    deps_url.searchParams.append('phase', 'configure');
+    deps_url.searchParams.append('phase', 'build');
+    deps_url.searchParams.append('phase', 'test');
   }
-}).then(function(data) {
-  if (graphstyle === null || graphstyle === 'auto') {
-    graphstyle = data.every(function(elem) { return elem.children.length < 10 ? true : false }) ? 'topdown' : 'concentric';
+  deps_url.searchParams.set('relationship', 'requires');
+  if (recommends) { deps_url.searchParams.append('relationship', 'recommends'); }
+  if (suggests) { deps_url.searchParams.append('relationship', 'suggests'); }
+  if (perl_version !== null && perl_version !== '') {
+    deps_url.searchParams.set('perl_version', perl_version);
   }
-  var elements = populate_graph(data);
-  create_graph(elements, graphstyle, dist);
-}).catch(function(error) {
-  console.log('Error retrieving dependencies', error);
-});
+  fetch(deps_url).then(function(response) {
+    if (response.ok) {
+      return response.json();
+    } else {
+      throw new Error(response.status + ' ' + response.statusText);
+    }
+  }).then(function(data) {
+    if (graphstyle === null || graphstyle === 'auto') {
+      graphstyle = data.every(function(elem) { return elem.children.length < 10 ? true : false }) ? 'topdown' : 'concentric';
+    }
+    var elements = populate_graph(data);
+    create_graph(elements, graphstyle, dist);
+  }).catch(function(error) {
+    console.log('Error retrieving dependencies', error);
+  });
+}
+
+retrieve_graph();
